e207137d46adbdeffa300a43b826c499357eba5a
frmnxhken <frmnxhken@gmail.com>
2025-03-07 00:38:02 +0700

    Improve response dialog

--- src/components/post/Responses.jsx
+++ src/components/post/Responses.jsx
@@ -8,7 +8,7 @@ const Responses = (props) => {
     <>
       <Overlay status={props?.active}/>
       <div className={`${props?.active ? 'scrollbar-hide' : 'bottom-[-100%] sm:right-[-100%]'} 
-        fixed transition-all bottom-0 right-0 z-50 bg-white rounded-t-3xl sm:rounded-t-none w-full sm:w-[25%] h-[90%] sm:h-full overflow-y-auto`}>
+        fixed transition-all bottom-0 right-0 z-50 bg-white rounded-t-2xl sm:rounded-t-none w-full sm:w-[25%] h-[95%] sm:h-full overflow-y-auto`}>
         <div className='py-12 px-6'>
             <div className='flex items-center justify-between mb-6'>
               <h1 className='text-2xl font-semibold'>Responses (17)</h1>
